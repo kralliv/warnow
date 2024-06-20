e62ae98b320b6e83b10a37c6435bd8e57024ae6b
Iven Krall <iven.krall@bewotec.de>
2024-06-20 15:17:47 +0200

    Some examples

--- src/main/kotlin/example.kt
+++ src/main/kotlin/example.kt
@@ -5,6 +5,73 @@ import java.awt.BorderLayout
 import javax.swing.JButton
 import javax.swing.JPanel
 
+class Example(context: Context) {
+
+    val flag by expect { flags.flag within context }
+    var text by define { text as String? initially null }
+
+    init {
+        // property access syntax
+        expect { some.nested.property }
+
+        // property definition syntax
+        define { some.nested.property as List<Int> initially null }
+
+        // value access syntax
+        access { authentication.username }
+
+        // value update syntax
+        mutate {
+            username = "kralli"
+        }
+
+        ::text.addInvalidationListener {}
+
+        define { ui.description as String initially "A Hello World Program" within context }
+    }
+}
+
+fun some(): Boolean {
+    return false
+}
+
+fun main() {
+    val context = createContextImplementation()
+    define { ui.message as String initially "Hello World" }
+
+    expect { ui.message }
+        .addInvalidationListener { text ->
+            print("context => ")
+            println(text)
+        }
+
+    mutate {
+        if (some()) {
+            return@mutate
+        }
+
+        ui(context) {
+            message = "Hello World 2"
+            dong
+        }
+    }
+
+    access {
+        ui(context) {
+            message
+        }
+    }
+
+    println("a")
+
+    Example(context)
+}
+
+fun none() {
+    define { ui.some as String initially "hallo" }
+}
+
+//some experimentation
 class ExamplePanel(context: NestedContext) : JPanel() {
 
     private val names: List<String> by warnow.state.names within context
@@ -36,4 +103,3 @@ class ExamplePanel(context: NestedContext) : JPanel() {
         add(resetButton, BorderLayout.EAST)
     }
 }
-
